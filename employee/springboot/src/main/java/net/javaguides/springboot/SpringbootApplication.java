58654a86b4c978949ac9186db4c7c938246d655c
sneha-r-225 <84561039+sneha-r-225@users.noreply.github.com>
2022-11-23 19:04:54 +0530

    Add files via upload

---
+++ employee/springboot/src/main/java/net/javaguides/springboot/SpringbootApplication.java
@@ -0,0 +1,45 @@
+package net.javaguides.springboot;
+
+import net.javaguides.springboot.entity.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Employee employee1 = Employee.builder()
+				.firstName("Ramesh")
+				.lastName("Fadatare")
+				.email("ramesh@gmail.com")
+				.build();
+
+		Employee employee2 = Employee.builder()
+				.firstName("Tony")
+				.lastName("Stark")
+				.email("tony@gmail.com")
+				.build();
+
+		Employee employee3 = Employee.builder()
+				.firstName("John")
+				.lastName("Cena")
+				.email("cena@gmail.com")
+				.build();
+
+		employeeRepository.save(employee1);
+		employeeRepository.save(employee2);
+		employeeRepository.save(employee3);
+	}
+}

45d1a28759980e21db77cc6bf4037b756a01e2b0
sneha-r-225 <84561039+sneha-r-225@users.noreply.github.com>
2022-11-23 18:57:36 +0530

    Add files via upload

---
+++ React Application  and spring boot/springboot/src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -0,0 +1,22 @@
+package net.javaguides.springboot.controller;
+
+import net.javaguides.springboot.entity.Employee;
+import net.javaguides.springboot.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("http://localhost:3000/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> fetchEmployees(){
+        return employeeRepository.findAll();
+    }
+}
